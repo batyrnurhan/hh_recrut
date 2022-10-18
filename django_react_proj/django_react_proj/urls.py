d421380b7e90af22366c000da149e879f769d3ea
batyrnurhan <72395356+batyrnurhan@users.noreply.github.com>
2022-10-18 12:12:54 +0600

    Update urls.py

--- django_react_proj/django_react_proj/urls.py
+++ django_react_proj/django_react_proj/urls.py
@@ -8,13 +8,16 @@ from materials.views import MaterialAPIList, MaterialAPIDestroy, MaterialAPIUpda
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
+    #materials api
     path('api/v1/materials/', MaterialAPIList.as_view()),
     path('api/v1/materials/<int:pk>/', MaterialAPIUpdate.as_view()),
     path('api/v1/material_delete/<int:pk>/', MaterialAPIDestroy.as_view()),
+    #teachers api -- gonna delete
     #path('api/v1/drf-auth/', include('rest_framework.urls')),
     #path('api/v1/teachers/', TeacherAPIList.as_view()),
     #path('api/v1/teachers/<int:pk>/', TeacherAPIUpdate.as_view()),
     #path('api/v1/teachersdelete/<int:pk>/', TeacherAPIDestroy.as_view()),
+    
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
